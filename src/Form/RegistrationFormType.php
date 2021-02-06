70cfdc79d1327ebff891c78ebbae62c79cbb1108
aymen ajroud <76960738+SharkInABottle@users.noreply.github.com>
2021-02-06 17:39:04 +0100

    initial commit

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Member;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('name')
+            ->add('lastName')
+            ->add('phoneNumber')
+            ->add('plainPassword', PasswordType::class, [ 'mapped' => false,'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Member::class,
+        ]);
+    }
+}
